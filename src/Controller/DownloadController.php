efa40cc8e02abe783232653d9f4ebbeb69d95487
andriramananarivo <andriramananarivo@gmail.com>
2026-06-21 19:39:06 +0200

    Génération pdf facture d'achat

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Document;
+use App\Entity\Invoice;
+use App\Service\CreditService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +14,19 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class DownloadController extends AbstractController
 {
+    public function __construct(
+        private readonly CreditService $creditService,
+        private readonly entityManagerInterface $em,
+    )
+    {
+
+    }
     #[Route('/download/{id}', name: 'app_download')]
     public function index(Document $document): Response
     {
+        $invoice = $this->em->getRepository(Invoice::class)->findOneBy(['document' => $document]);
+        $this->creditService->generatePDF($invoice);
+
         $path = sprintf("%s/%s",
             $this->getParameter("documents_directory"),
             $document->getId()
